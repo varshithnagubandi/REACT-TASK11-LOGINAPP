f9e5ac9dc12aaf57241b5c2f7e6516cf9e97c240
vvvarshith <vvvarshith.nagubandi@gmail.com>
2024-08-07 02:24:05 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const inputhandle = (event) => {
+    setData({ ...data, [event.target.name]: [event.target.value] });
+  };
+
+  const submithandler = (event) => {
+    event.preventDefault();
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box1">
+      <center>
+        <h1 className="heading">LOGIN FORM</h1>
+
+        <form onSubmit={submithandler}>
+          <label>USERNAME :</label>
+          <input
+            type="text"
+            name="username"
+            value={data.username}
+            onChange={inputhandle}
+          />
+          <br />
+          <label>PASSWORD :</label>
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={inputhandle}
+          />
+          <br />
+          <button type="submit">SUBMIT</button>
+        </form>
+        <p className="sub-para">----- INSPECT AND SEE THE SAVED DATA -----</p>
+      </center>
     </div>
   );
 }
